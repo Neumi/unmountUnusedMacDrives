7c7b4b8e5f6d98000b680bc3bda0b0dcaee5a4f2
Neumi <jan.neumann111@googlemail.com>
2017-04-21 19:17:28 +0200

    added shell script
    this is the script you need to unmount the HDDs

---
+++ unmountHDD.sh
@@ -0,0 +1,12 @@
+
+diskutil mount /dev/disk0s2
+diskutil eject /dev/disk0s2
+
+diskutil mount /dev/disk1s2
+diskutil eject /dev/disk1s2
+
+diskutil mount /dev/disk2s2
+diskutil eject /dev/disk2s2
+
+echo "HDD has been ejected sucessfully! Thanks. Goodbye."
+
